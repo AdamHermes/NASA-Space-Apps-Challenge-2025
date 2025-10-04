0057b3e90b4688883f4c4135a17cadc39bf70db3
NamQuanProject <namquanproject@gmail.com>
2025-10-04 11:44:02 +0700

    Format the settings for process data

--- app/service/ml/inference.py
+++ app/service/ml/inference.py
@@ -1,8 +1,7 @@
+import pandas as pd
+from io import BytesIO
+from split import split
 
 
-
-
-
-def predict_sentiment(text: str):   
-    return {"output": "OKE", "status" : "200"}
-
+def inference(training_data, testing_data, sattelite_type):
+    pass 
--- app/service/ml/router.py
+++ app/service/ml/router.py
@@ -1,12 +1,18 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 from service.ml.inference import predict_sentiment
+from fastapi import APIRouter, UploadFile, File
+import pandas as pd
 
 router = APIRouter()
 
-class TextInput(BaseModel):
-    text: str
-
 @router.post("/predict")
-def predict(input: TextInput):
-    return predict_sentiment(input.text)
+async def predict(file: UploadFile = File(...)):
+    if not file.filename.endswith(".csv"):
+        return {"error": "Only CSV files are supported", "status": "400"}
+
+    contents = await file.read()
+    df = pd.read_csv(pd.io.common.BytesIO(contents))
+
+    pass
+
