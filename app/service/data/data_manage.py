747ce5de98fcfc81d43c07f5c0b5f44f291d1bd4
NamQuanProject <namquanproject@gmail.com>
2025-10-04 17:25:12 +0700

    Adding weights getting models

--- app/service/data/data_manage.py
+++ app/service/data/data_manage.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 
 
@@ -11,7 +12,18 @@ def get_current_csv_files():
 
 
 def get_current_models():
-    return os.listdir("app/storage/weights")
+    base_dir = "app/storage/weights"
+    models_info = {}
 
+    for model_name in os.listdir(base_dir):
+        model_path = os.path.join(base_dir, model_name)
+        if os.path.isdir(model_path):
+            weights = [
+                f for f in os.listdir(model_path)
+                if os.path.isfile(os.path.join(model_path, f))
+            ]
+            models_info[model_name] = weights
+
+    return json.dumps(models_info, indent=4)
 
 
