2ea38238d98f5166a7d54057d2b3441854b9de2d
dinhhogiahuy290605@gmail.com <dinhhogiahuy290605@gmail.com>
2025-10-04 19:43:33 +0700

    get all hostnames endpoint

--- app/main.py
+++ app/main.py
@@ -1,13 +1,22 @@
 from fastapi import FastAPI
 from app.routes import router
 from .routers import data, ml_routers, visualization
-
+from fastapi.middleware.cors import CORSMiddleware
 # Create FastAPI app
 app = FastAPI(
     title="My FastAPI App",
     description="A sample FastAPI project with app folder",
     version="1.0.0"
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Your frontend URL
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],  # Include OPTIONS
+    allow_headers=["*"],
+)
+
     
 
 app.include_router(router)
